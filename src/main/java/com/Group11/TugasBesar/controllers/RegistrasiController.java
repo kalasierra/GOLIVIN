3c03d9176cb887a0014c82da4a934fcf1dfea52f
Hydro2Oxide-cmd <141400721+Hydro2Oxide-cmd@users.noreply.github.com>
2023-12-14 21:07:53 +0700

    merapikan registrasicontroller

--- src/main/java/com/Group11/TugasBesar/controllers/RegistrasiController.java
+++ src/main/java/com/Group11/TugasBesar/controllers/RegistrasiController.java
@@ -4,19 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.Group11.TugasBesar.payloads.requests.BookRequest;
 import com.Group11.TugasBesar.payloads.requests.UserRequest;
 import com.Group11.TugasBesar.payloads.responses.Response;
-import com.Group11.TugasBesar.services.book.BookService;
 import com.Group11.TugasBesar.services.user.UserService;
-
-import org.springframework.ui.Model;
+;
 
 @SpringBootApplication
 @Controller
@@ -29,13 +24,6 @@ public class RegistrasiController {
     public String registrasiPage() {
         return "registrasi";
     }
-
-    // @RequestMapping(value = "registrasi", method = RequestMethod.POST)
-    // public String homepage2Page(@RequestParam String username, @RequestParam
-    // String password, @RequestParam String phoneNumber){
-    // System.out.println(username);
-    // return "homepage2";
-    // }
 
     @RequestMapping(value = "registrasi", method = RequestMethod.POST, consumes = "application/x-www-form-urlencoded")
     public ResponseEntity<?> homepage2Page(UserRequest userRequest) {
