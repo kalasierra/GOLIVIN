ccabc1b097ac862649c598f58f23fcc9ff50ac4c
aliciaparinussa <aliciaparinussa07@gmail.com>
2023-12-14 08:37:26 +0700

    registrasi

---
+++ src/main/java/com/Group11/TugasBesar/controllers/RegistrasiController.java
@@ -0,0 +1,22 @@
+package com.Group11.TugasBesar.controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+public class RegistrasiController {
+    @RequestMapping(value = "/registrasi", method = RequestMethod.GET)
+
+    public String registrasiPage(){
+        return "/registrasi"
+    }
+
+    @RequestMapping(value = "/registrasi", method = RequestMethod.POST)
+
+    public String homepage2Page(Model model @RequestParam String nama, @RequestParam String nomorHP, @RequestParam String Email, @RequestParam String pass, @RequestParam String pass){
+        return "homepage2"
+    }
+}
