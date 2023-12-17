98dc746e42dab28c8bbaa215e80385196c244672
Hydro2Oxide-cmd <141400721+Hydro2Oxide-cmd@users.noreply.github.com>
2023-12-17 11:09:48 +0700

    Nambah file untuk test JSP
    disini ada controller baru untuk JSP. Berguna kalau mau lihat interface JSP nya. untuk menggunakannya, ubah "test" di file TestController.java ke direktory JSP yg baru. lalu untuk mencobanya di browser, ketik localhost:8080/test

---
+++ src/main/java/com/Group11/TugasBesar/controllers/TestController.java
@@ -0,0 +1,17 @@
+package com.Group11.TugasBesar.controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String registerPage() {
+
+        return "test";
+    }
+}
