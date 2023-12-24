3e00366b8fe03ca05facc843a6ab75c6013567fa
Hydro2Oxide-cmd <141400721+Hydro2Oxide-cmd@users.noreply.github.com>
2023-12-24 13:05:42 +0700

    Nambah fungsi untuk ngecek apakah user sudah terdaftar atau belum

--- src/main/java/com/Group11/TugasBesar/services/pencariKost/PencariKostServiceImpl.java
+++ src/main/java/com/Group11/TugasBesar/services/pencariKost/PencariKostServiceImpl.java
@@ -1,45 +1,73 @@
 package com.Group11.TugasBesar.services.pencariKost;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.Group11.TugasBesar.models.PemilikKost;
 import com.Group11.TugasBesar.models.PencariKost;
 import com.Group11.TugasBesar.models.User;
 import com.Group11.TugasBesar.payloads.requests.UserRequest;
 import com.Group11.TugasBesar.payloads.responses.Response;
 import com.Group11.TugasBesar.repositories.PencariKostRepository;
 import com.Group11.TugasBesar.repositories.UserRepository;
+import com.Group11.TugasBesar.services.user.UserService;
 
 @Service
 public class PencariKostServiceImpl implements PencariKostService{
+
+	@Autowired
+	private UserService userService;
+
     @Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private PencariKostRepository pencariKostRepository;
 
+	@Override
 	public Response addPencariKost(UserRequest request) {
-		User user = new User();
-		PencariKost pencariKost = new PencariKost();
-		pencariKost = pencariKostRepository.save(pencariKost);
+		try {
+			Response response = userService.getUserByEmail(request.getEmail());
 
-		user.setUsername(request.getUsername());
-		user.setPhoneNumber(request.getPhoneNumber());
-		user.setEmail(request.getEmail());
-		user.setPassword(request.getPassword());
-		user.setPencariKost(pencariKost);
-		user = userRepository.save(user);
+			response = new Response();
+			response.setStatus(HttpStatus.CONFLICT.value());
+			response.setMessage("User already exist!");
+			response.setData(null);
 
-		Response response = new Response();
-		response.setStatus(HttpStatus.CREATED.value());
-		response.setMessage("User created successfully!");
-		response.setData(user);
+			return response;
+		} catch (NoSuchElementException e) {
 
-		return response;
+			User user = new User();
+			PencariKost pencariKost = new PencariKost();
+			pencariKost = pencariKostRepository.save(pencariKost);
+
+			user.setUsername(request.getUsername());
+			user.setPhoneNumber(request.getPhoneNumber());
+			user.setEmail(request.getEmail());
+			user.setPassword(request.getPassword());
+			user.setPencariKost(pencariKost);
+			user = userRepository.save(user);
+
+			Response response = new Response();
+			response.setStatus(HttpStatus.CREATED.value());
+			response.setMessage("User created successfully!");
+			response.setData(user);
+
+			return response;
+		} catch (Exception e) {
+			Response response = new Response();
+			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+			response.setMessage("Internal Server Error");
+			response.setData(null);
+			return response;
+		}
 	}
 
+	@Override
   	public Response getPencariKost() {
 		List<PencariKost> pencariKosts = pencariKostRepository.findAll();
 
